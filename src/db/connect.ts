540bfa4c40a5b9696510bb85991a579273b15f3b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-20 23:25:09 +0200

    feat: import dotenv for env files

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,6 +1,5 @@
-import "dotenv/config";
 import mongoose from "mongoose";
-
+import "dotenv/config";
 const dbConnect = async () => {
   const connectionString = process.env.DATABASE_URL;
   return mongoose.connect(connectionString);
