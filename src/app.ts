f81f4847b3352c46f524dedde5dc9d8962ab1f0e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-21 02:51:47 +0200

    fix: import express-async-erros in the first line to fix an error with error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import "express-async-errors";
 import express from "express";
 import notFoundMiddleware from "./middleware/not-found";
 import errorHandlerMiddleware from "./middleware/error-handler";
