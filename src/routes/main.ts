9f1f3d5877c1677befa70a37eec1c5f8c7335282
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-22 00:18:33 +0200

    feat: use auth middleware in dashboard route

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { login, dashboard } from "../controllers/main";
+import authMiddleware from "../middleware/auth";
+import validateLogin from "../middleware/joi-validation";
 const router = express.Router();
 
-router.route("/dashboard").get(dashboard);
-router.route("/login").post(login);
+router.route("/dashboard").get(authMiddleware, dashboard);
+router.route("/login").post(validateLogin, login);
 
 export default router;
