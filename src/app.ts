c8e8919aaaaf5c7107684937d15c22862cf4c0b2
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-22 00:16:18 +0200

    refactor: move validateLoginQuery middleware to router

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,12 @@ import notFoundMiddleware from "./middleware/not-found";
 import errorHandlerMiddleware from "./middleware/error-handler";
 // import dbConnect from "./db/connect";
 import router from "./routes/main";
-import validateLoginQuery from "./middleware/joi-validation";
+
 const app = express();
 
 // middleware
 app.use(express.static("./src/public"));
 app.use(express.json());
-app.use("/api/v1/login", validateLoginQuery);
 app.use("/api/v1", router);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
