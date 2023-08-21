f1ca0c7d13ebd35224e3a704760b5ecdbb6bdbd8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-22 00:57:22 +0200

    fix: use correct status codes

--- src/errors/unauthenticated.ts
+++ src/errors/unauthenticated.ts
@@ -3,7 +3,7 @@ import { StatusCodes } from "http-status-codes";
 
 class BadRequest extends CustomAPIError {
   constructor(public message: string) {
-    super(message, StatusCodes.BAD_REQUEST);
+    super(message, StatusCodes.UNAUTHORIZED);
   }
 }
 
--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { CustomAPIError } from "../errors";
+import { StatusCodes } from "http-status-codes";
 const errorHandlerMiddleware = (
   err: Error,
   _req: Request,
@@ -10,7 +11,7 @@ const errorHandlerMiddleware = (
     return res.status(err.statusCode).json({ msg: err.message });
   }
   return res
-    .status(500)
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
     .json({ message: "Something went wrong try again later", error: err });
 };
 
