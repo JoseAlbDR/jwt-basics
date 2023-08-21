0bcc20d1a07e5bf066bbd70302f66ce16d25c331
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-21 18:38:15 +0200

    feat: Joi validation middleware to validate req.body data and return a custom error or a req.loginQuery

--- src/middleware/joi-validation.ts
+++ src/middleware/joi-validation.ts
@@ -1,1 +1,26 @@
+import { NextFunction, Response } from "express";
+import { validateUserData } from "../utils/validation";
+import CustomAPIError from "../errors/custom-error";
+import { ILoginRequest } from "../types/interfaces";
 
+const validateLoginQuery = (
+  req: ILoginRequest,
+  _res: Response,
+  next: NextFunction
+) => {
+  const { body } = req;
+  console.log(body);
+  const valid = validateUserData(body);
+
+  if (valid.error) {
+    const messages = valid.error.details.map(
+      (detail): string => detail.message
+    );
+    throw new CustomAPIError(messages.join(" "), 400);
+  }
+
+  req.loginQuery = valid.value;
+  return next();
+};
+
+export default validateLoginQuery;
