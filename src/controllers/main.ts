a3e1885feb7598f0f58f9cbd23b9fef6df681ea7
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-21 02:52:07 +0200

    feat: throw new custom error it here is no username or no password

--- src/controllers/main.ts
+++ src/controllers/main.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from "express";
+import { ILoginRequest } from "../types/interfaces";
+import CustomAPIError from "../errors/custom-error";
 
-export const login = async (_req: Request, res: Response) => {
+export const login = async (req: ILoginRequest, res: Response) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new CustomAPIError("Invalid username or password", 400);
+  }
   res.send("Fake Login/Register/Signup");
 };
 
