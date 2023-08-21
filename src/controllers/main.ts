f104efea70295c14ca5e0619889cf29e5a5f20e2
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-21 19:05:17 +0200

    feat: create new JWT

--- src/controllers/main.ts
+++ src/controllers/main.ts
@@ -1,14 +1,26 @@
+import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
 import { ILoginRequest } from "../types/interfaces";
+import("dotenv/config");
 
 export const login = async (req: ILoginRequest, res: Response) => {
-  const { username, password } = req.loginQuery;
-  res
-    .status(200)
-    .json({
-      message: "Fake Login/Register/Signup",
-      data: { username, password },
+  const { username } = req.loginQuery;
+
+  // just for demo, normally provided by DB
+  const id = new Date().getDate();
+
+  try {
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
     });
+
+    res.status(200).json({
+      message: "User created",
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const dashboard = async (_req: Request, res: Response) => {
