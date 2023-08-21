e61f8caaf7d927445eb4668e04427156bfe2f1d0
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-21 18:37:34 +0200

    feat: validateUserData function to validate userdata from req.body with Joi

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,3 +1,11 @@
+import Joi from "joi";
+import { ILoginBody } from "../types/interfaces";
 
+export const validateUserData = (userData: unknown) => {
+  const loginSchema = Joi.object<ILoginBody>({
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  });
 
-
+  return loginSchema.validate(userData, { errors: { wrap: { label: false } } });
+};
